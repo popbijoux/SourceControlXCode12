b05cd81d51f5bd711eaa180ce35185f7c399b0e0
Manuela Leal <popbijoux@gmail.com>
2022-04-11 14:13:44 -0400

    Initial Commit

---
+++ SourceControlXCode12/SourceControlXCode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXCode12App.swift
+//  SourceControlXCode12
+//
+//  Created by Manuela Leal on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXCode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
